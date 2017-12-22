06f7bbedc794a16fcd514f03155c6820fd57c069
Ivan Solomichev <vanijatko.social@gmail.com>
2017-12-22 14:59:57 +0200

    added visibility modifiers

--- instant_token/contracts/SafeMath.sol
+++ instant_token/contracts/SafeMath.sol
@@ -1,22 +1,22 @@
 pragma solidity ^0.4.17;
 
 library SafeMath {
-  function mul(uint a, uint b) internal returns (uint) {
+  function mul(uint a, uint b) internal pure returns (uint) {
     uint c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
-  function div(uint a, uint b) internal returns (uint) {
+  function div(uint a, uint b) internal pure returns (uint) {
     assert(b > 0);
     uint c = a / b;
     assert(a == b * c + a % b);
     return c;
   }
-  function sub(uint a, uint b) internal returns (uint) {
+  function sub(uint a, uint b) internal pure returns (uint) {
     assert(b <= a);
     return a - b;
   }
-  function add(uint a, uint b) internal returns (uint) {
+  function add(uint a, uint b) internal pure returns (uint) {
     uint c = a + b;
     assert(c >= a);
     return c;
